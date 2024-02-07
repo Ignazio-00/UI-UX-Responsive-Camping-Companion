5c183c338a75015718d92e868c77932fc0c5e94b
Ignazio Balistreri <ignazio.balistreri@code.berlin>
2024-02-07 18:48:23 +0100

    Add constants

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,6 +6,9 @@ const Navbar = () => {
     <nav className="flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
         <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
+        <ul className="hidden h-full gap-12 lg:flex">
+          
+        </ul>
       </Link>
     </nav>
   );
