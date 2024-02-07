119635b9e13c5a2e1687c76671af4277d700898e
Ignazio Balistreri <ignazio.balistreri@code.berlin>
2024-02-07 19:43:36 +0100

    Add hamburger menu functionality

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,29 @@
+"use client";
+
 import { NAV_LINKS } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 import Button from "./Button";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
         <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
       </Link>
 
-      <ul className="hidden h-full gap-12 lg:flex">
+      <ul
+        className={`lg:flex flex-col lg:flex-row lg:h-full gap-12 ${
+          isOpen ? "flex" : "hidden"
+        } lg:static absolute top-full left-0 w-full bg-gray-800 p-4 lg:bg-transparent lg:p-0 transition-all duration-300`}
+      >
         {NAV_LINKS.map((link) => (
           <Link
             href={link.href}
@@ -29,13 +42,17 @@ const Navbar = () => {
           variant="btn_dark_green"
         />
       </div>
-      <Image
-        src="menu.svg"
-        alt="menu"
-        width={32}
-        height={32}
+      <div
         className="inline-block cursor-pointer lg:hidden"
-      />
+        onClick={toggleMenu}
+      >
+        <Image
+          src={isOpen ? "/x.png" : "/menu.svg"}
+          alt="menu"
+          width={32}
+          height={32}
+        />
+      </div>
     </nav>
   );
 };
