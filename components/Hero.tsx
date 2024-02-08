4fb1d8b90112c49414607ae3f58cad0064c9e352
Ignazio Balistreri <ignazio.balistreri@code.berlin>
2024-02-08 12:37:15 +0100

    Add Hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,78 @@
+import Image from "next/image";
 import React from "react";
+import Button from "./Button";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="py-10 max-container padding-container flex flex-col gap-20 pb-32 md:gap-28 lg:py-20 xl:flex-row">
+      <div className="hero-map" />
+      <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
+        <Image
+          src="/camp.svg"
+          alt="camp"
+          width={50}
+          height={50}
+          className="absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]"
+        />
+        <h1 className="bold-52 lg:bold-88">Camping made easy.</h1>
+        <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
+          Create your dream camping route and make camping fun again. We help
+          you find the perfect camping spot to you adventure nature.
+        </p>
+        <div className="my-11 flex flex-wrap gap-5">
+          <div className="flex items-center gap-2">
+            {Array(5)
+              .fill(1)
+              .map((_, index) => (
+                <Image
+                  key={index}
+                  src="/star.svg"
+                  alt="star"
+                  width={24}
+                  height={24}
+                />
+              ))}
+          </div>
+          <p className="bold-16 lg:bold-20 text-blue-70">
+            100k
+            <span className="regular-16 lg:regular-20 ml-1"> reviews</span>
+          </p>
+        </div>
+        <div className="flex flex-col w-full gap-3 sm:flex-row">
+          <Button type="button" title="Dowlnoad App" variant="btn_green" />
+          <Button
+            type="button"
+            title="Show me"
+            icon="/play.svg"
+            variant="btn_white_text"
+          />
+        </div>
+      </div>
+      <div className="relative flex flex-1 items-start">
+        <div className="relative z-20 w-[268px] flex flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8">
+          <div className="flex flex-col">
+            <div className="flexBetween">
+              <p className="regular-16 text-gray-20">Location</p>
+              <Image src="/close.svg" alt="close" width={24} height={24} />
+            </div>
+            <p className="bold-20 text-white">Camping Location</p>
+          </div>
+          <div className="flexBetween">
+            <div className="flex flex-col">
+              <p className="regular-16 text-gray-20 block">DISTANCE</p>
+              <p className="bold-20 text-white">970m</p>
+            </div>
+            <div className="flexBetween">
+              <div className="flex flex-col">
+                <p className="regular-16 text-gray-20 block">Elevation</p>
+                <p className="bold-20 text-white">2.013km</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
