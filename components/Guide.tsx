f210fc5695d0dddf08b0dc79c0d90a50e85876ae
Ignazio Balistreri <ignazio.balistreri@code.berlin>
2024-02-08 22:41:33 +0100

    Add Guide section

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -1,7 +1,59 @@
-import React from "react";
+import Image from "next/image";
 
 const Guide = () => {
-  return <div>Guide</div>;
+  return (
+    <section className="flexCenter flex-col">
+      <div className="padding-container max-container w-full pb-24">
+        <Image src="/camp.svg" alt="camp" width={50} height={50} />
+        <p className="uppercase regular-18 -mt-1 mb-3 text-green-50">
+          Camping Companion
+        </p>
+        <div className="flex flex-wrap justify-between gap-5 lg:gap-10">
+          <h2 className="bold-40 lg:bold-64 xl:max-w-[390px]">
+            Explore the Great Outdoors!
+          </h2>
+          <p className="regular-16 text-gray-30 xl:max-w-[520px]">
+            Whether you're a seasoned outdoor enthusiast or just starting your
+            journey, our app is your go-to resource for planning, navigating,
+            and experiencing the wilderness. Discover hidden gems, find the
+            perfect campsites, and access essential tools and tips to make every
+            trip a memorable one. Download now and start your next adventure!
+          </p>
+        </div>
+      </div>
+      <div className="flexCenter max-container relative w-full">
+        <Image
+          src="/boat.png"
+          alt="boat"
+          width={1440}
+          height={580}
+          className="w-full object-cover object-center 2xl:rounded-5xl"
+        />
+        <div className="absolute flex bg-white py-8 pl-5 pr-7 gap-3 rounded-3xl border shadow-md md:left-[5%] lg:top-20">
+          <Image
+            src="/meter.svg"
+            alt="meter"
+            width={16}
+            height={158}
+            className="h-full w-auto"
+          />
+          <div className="flexBetween flex-col">
+            <div className="flex w-full flex-col">
+              <div className="flexBetween w-full">
+                <p className="regular-16 text-gray-20">Destination</p>
+                <p className="bold-16 text-green-50">48 min</p>
+              </div>
+              <p className="bold-20 mt-2">Camping Spot</p>
+            </div>
+            <div className="flex w-full flex-col">
+              <p className="regular-16 text-gray-20">Start Track</p>
+              <h4 className="bold-20 mt-2 whitespace-nowrap">Camping Spot 2</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Guide;
