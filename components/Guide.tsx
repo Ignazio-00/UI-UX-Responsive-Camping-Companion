c33b821385d4468df1b56bff20859a713510c251
Ignazio Balistreri <ignazio.balistreri@code.berlin>
2024-03-04 12:07:45 +0100

    Fix: Footer section

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -41,7 +41,7 @@ const Guide = () => {
             <div className="flex w-full flex-col">
               <div className="flexBetween w-full">
                 <p className="regular-16 text-gray-20">Destination</p>
-                <p className="bold-16 text-green-50">48 min</p>
+                <p className="bold-16 text-green-50">32 min</p>
               </div>
               <p className="bold-20 mt-2">Camping Spot A</p>
             </div>
